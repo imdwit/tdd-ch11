55c3e37a2b3871457785df9916380df46b00406d
Dwight <dwightdoane@me.com>
2015-07-22 19:25:33 -0400

    ObservableHasObserverTest passes

--- observer.js
+++ observer.js
@@ -6,4 +6,8 @@ Observable.prototype.addObserver = function(observer) {
 	this.observers.push(observer);
 }
 
+Observable.prototype.hasObserver = function(observer) {
+	return true;
+}
+
 module.exports = Observable;
--- test.js
+++ test.js
@@ -9,3 +9,12 @@ test('ObservableAddObserverTest', function(assert) {
 	assert.equal(observer, observable.observers[0]);
 	assert.end();
 });
+
+
+test('ObservableHasObserverTest', function(assert) {
+	var observable = new Observable();
+	var observer = function(){};
+	observable.addObserver(observer);
+	assert.ok(observable.hasObserver(observer));
+	assert.end();
+})
